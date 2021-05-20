7e2750ca284d3bec9ffedb4a57e659483159c92b
thakaj <taulant.hakaj@scranton.edu>
2021-05-20 19:37:04 -0400

    commit tree cd7c228dcc6c52916b92cd41d249d552a9dbe4b8

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function saturdayFun(input="roller-skate"){
+    return `This Saturday, I want to ${input}!`
+}
+function mondayWork(input="go to the office")  {
+    return `This Monday, I will ${input}.`
+} 
+function wrapAdjective(input){
+    const adjective = `special`;
+    return function(){
+        console.log(`You are ${adjective}`)
+    }
+}
